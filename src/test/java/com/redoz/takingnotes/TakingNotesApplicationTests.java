9e30e30753b7c75187c4eff9f6628dac1ec4dc1b
Nicolás <yohannicolas2003@gmail.com>
2023-07-24 17:15:14 -0500

    initial commit

---
+++ src/test/java/com/redoz/takingnotes/TakingNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redoz.takingnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakingNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
